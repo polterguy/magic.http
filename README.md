e8e09be79457071858527e6f4e99d3ff869733b0
Thomas Hansen <thomas@gaiasoul.com>
2019-09-27 18:25:33 +0300

    Cleanups

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 
-# Magic Http for .Net
+# Magic HTTP for .Net
 
 [![Build status](https://travis-ci.org/polterguy/magic.http.svg?master)](https://travis-ci.org/polterguy/magic.http)
 
-To be provided ...
+A minimalistic HTTP REST library for .Net built for .Net Standard. The whole idea with the library, is to provide a
+single line invocation for HTTP REST invocations from C# and other CLR languages. Below you can see an example of usage.
+
+```csharp
+var result = await client.GetAsync<Blog[]>("https://my-json-server.typicode.com/typicode/demo/posts");
+```
+
+The idea is tha tyou provide your request type (if any), and your response type as generic arguments to its
+`IHttpClient` interface, and the library will perform automatic conversion on your behalf, reducing your
+HTTP REST invocations to a single line of code, resembling _"normal method invocations"_.
+
+The library supports all 4 most commonly verbs, below is a list.
+
+* POST
+* GET
+* PUT
+* DELETE
+
+The `POST` and `PUT` verbs requires payload objects, that you provide as typed generic arguments, while the `DELETE` and `GET`
+verbs does not. In addition you can optionally supply a _"token"_, normally a JWT token, that will be transmitted in the
+HTTP Authorize header of your request, as a _"Bearer"_ token. In addition the library will _"intelligently"_ handle both `Stream`
+requests and `Stream` responses for POST, PUT and GET (not DELETE), allowing you to either supply a `Stream` as a request
+object, which will serialize your stream directly on to the HTTP request stream, or by using the overload of the `GetAsync`
+method that requires you to supply an `Action` taking one stream, from where you can directly access the HTTP response stream.
+This allows you to serialize large files, without first loading them into memory.
+
+Apart from the above features, the library does not really give you much options, and is to be considered an _"opinionated"_
+HTTP REST library, and its purpose is not to support every possible configuration you can imagine, since its purpose
+is first and foremost to be dead simple, yet still hyper optimal in usage, taking advantage of the _"newer"_ stuff
+from .Net, such as `HttpClient` (that it's using internally), and async methods.
--- magic.http.contracts/IHttpClient.cs
+++ magic.http.contracts/IHttpClient.cs
@@ -9,16 +9,63 @@ using System.Threading.Tasks;
 
 namespace magic.http.contracts
 {
+    /// <summary>
+    /// Interface for accessing your HttpClient.
+    /// </summary>
     public interface IHttpClient
     {
+        /// <summary>
+        /// Posts an object asynchronously to the specified URL. Notice, you can supply a Stream as your request,
+        /// and the service will intelligently determine it's a stream, and serialize it directly on to the HTTP request stream.
+        /// </summary>
+        /// <typeparam name="Request">Type of request.</typeparam>
+        /// <typeparam name="Response">Type of response.</typeparam>
+        /// <param name="url">URL of your request.</param>
+        /// <param name="request">Payload of your request.</param>
+        /// <param name="token">Optional Bearer token for your request.</param>
+        /// <returns>Object returned from your request.</returns>
         Task<Response> PostAsync<Request, Response>(string url, Request request, string token = null);
 
+        /// <summary>
+        /// Puts an object asynchronously to the specified URL. Notice, you can supply a Stream as your request,
+        /// and the service will intelligently determine it's a stream, and serialize it directly on to the HTTP request stream.
+        /// </summary>
+        /// <typeparam name="Request">Type of request.</typeparam>
+        /// <typeparam name="Response">Type of response.</typeparam>
+        /// <param name="url">URL of your request.</param>
+        /// <param name="request">Payload of your request.</param>
+        /// <param name="token">Optional Bearer token for your request.</param>
+        /// <returns>Object returned from your request.</returns>
         Task<Response> PutAsync<Request, Response>(string url, Request request, string token = null);
 
+        /// <summary>
+        /// Gets a resource from some URL.
+        /// </summary>
+        /// <typeparam name="Response">Type of response.</typeparam>
+        /// <param name="url">URL of your request.</param>
+        /// <param name="token">Optional Bearer token for your request.</param>
+        /// <returns>Object returned from your request.</returns>
         Task<Response> GetAsync<Response>(string url, string token = null);
 
+        /// <summary>
+        /// Gets a resource from some URL. Notice, this overload requires you to supply
+        /// an Action taking a Stream as its input, from where you can directly access the response content,
+        /// without having to load it into memory. This i suseful for downloading larger documents from some URL.
+        /// </summary>
+        /// <param name="url">URL of your request.</param>
+        /// <param name="functor">Action lambda function given the response Stream for you to do whatever you wish with once the request returns.</param>
+        /// <param name="accept">Optional Accept HTTP header declaration, allowing you to explicitly declare what type of MIME content you can handle.</param>
+        /// <param name="token">Optional Bearer token for your request.</param>
+        /// <returns>Async void Task</returns>
         Task GetAsync(string url, Action<Stream> functor, string accept = null, string token = null);
 
+        /// <summary>
+        /// Deletes some resource.
+        /// </summary>
+        /// <typeparam name="Response">Type of response.</typeparam>
+        /// <param name="url">URL of your request.</param>
+        /// <param name="token">Optional Bearer token for your request.</param>
+        /// <returns>Result of your request.</returns>
         Task<Response> DeleteAsync<Response>(string url, string token = null);
     }
 }
