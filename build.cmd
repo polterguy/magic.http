03e2b479d4da74214e798358f5ec451cc9ebaf19
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:57:04 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ set version=%1
 set key=%2
 
 cd %~dp0
-
 dotnet build magic.http.contracts/magic.http.contracts/magic.http.contracts.csproj --configuration Release --source https://api.nuget.org/v3/index.json
 dotnet nuget push magic.http.contracts/magic.http.contracts/bin/Release/magic.http.contracts.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
 
+cd %~dp0
 dotnet build magic.http.services/magic.http.services/magic.http.services.csproj --configuration Release --source https://api.nuget.org/v3/index.json
 dotnet nuget push magic.http.services/magic.http.services/bin/Release/magic.http.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
